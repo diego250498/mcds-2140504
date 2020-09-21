1201e816f2aef75573c449417e9d40eb4ca0f76f
Diego Alejandro Gaviria Alzate <gaviriadiego713@gmail.com>
2020-09-21 08:51:58 -0500

    'trabajo'

--- larapp/routes/web.php
+++ larapp/routes/web.php
@@ -24,9 +24,9 @@ Route::get('hellowworld', function () {
 Route::get('users', function () {
     $users = App\User::all()->take(10);
 
-    foreach($user as $user){
+    foreach($users as $user){
         $user->age = data_diff(data_create($user->birthdate), data_create(now))->format('%y');
-        $yearDiff = striftime("%Y", now()->getTimestamp()) - striftime("%Y", $used->created_at->getTimestamp());
+        $yearDiff = striftime("%Y", now()->getTimestamp()) - strftime("%Y", $user->created_at->getTimestamp());
         $weekDiff = strftime("%W", now()->getTimestamp()) - strftime("%W", $user->created_at->getTimestamp());
 
         $created = (($yearDiff >0) ? $yearDiff.'años':'').
